ed9e0eb5ea7b20388ca41ba3b744de28d5e630f0
Svetozar Mitrov <svetkobeast@gmail.com>
2017-11-05 21:32:36 +0200

    Initial commit.

---
+++ GeometryCalculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option(triangle, square, radians)");
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "triangle":
+                    Console.WriteLine("Enter side:");
+                    int side = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter heigh:");
+                    int heigh = int.Parse(Console.ReadLine());
+                    triangleMethod(side, heigh);
+                    break;
+                case "square":
+                    Console.WriteLine("Enter side:");
+                    int sideOfSquare = int.Parse(Console.ReadLine());
+                    squareMethod(sideOfSquare);
+                    break;
+                case "radians":
+                    Console.WriteLine("Enter the degrees you want to transform in radians:");
+                    int radians = int.Parse(Console.ReadLine());
+                    RadiansMethod(radians);
+                    break;
+                default:
+                    Console.WriteLine("Try again!");
+                    break;
+
+            }
+
+
+        }
+        static void triangleMethod(int side, int heigh)
+        {
+
+            double triangleArea = (side * heigh) / 2.0;
+            Console.WriteLine("The area of the triangle is = {0:F2}", triangleArea);
+        }
+        static void squareMethod(int sideOfSquare)
+        {
+            double squareArea = Math.Pow(sideOfSquare, 2);
+            Console.WriteLine("The area of the square is = {0:F2}.", squareArea);
+        }
+        static void RadiansMethod(int radians)
+        {
+            double pi = Math.PI;
+            double rad = (pi / 180) * radians;
+            Console.WriteLine("Your degrees in radians is = {0}", rad);
+        }
+    }
+}
---
+++ GetMax/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number:");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your third number:");
+            int num3 = int.Parse(Console.ReadLine());
+            int max = Math.Max(num1, Math.Max(num2, num3));
+            Console.WriteLine("Your biggest number is {0}", max);
+        }
+    }
+}
